5e6676f89d1c1edee92890e8692559231324a7d7
Emma Paris <mayankpurohit2012@gmail.com>
2020-10-05 14:07:06 +0530

    [Mayank] Add TictacToe class

---
+++ TicTacToeGame.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TICTacToeGame
+{
+    class TicTacToeGame
+    {
+        public char[] board = new char[10];
+
+
+        public TicTacToeGame()
+        {
+            for (int i = 0; i < 10; i++)
+                board[i] = ' ';
+        }
+    }
+}
